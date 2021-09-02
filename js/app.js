6dfd9add50eff77d0942b28cf7cc5ed3ffd73da4
keylighter <keysuggest@gmail.com>
2021-09-02 12:41:11 +0600

    search boxfetch

--- js/app.js
+++ js/app.js
@@ -1,1 +1,14 @@
-console.log('hello');
+const searchResult = document.getElementById('books-result');
+const searchField = document.getElementById('search-field');
+
+
+// searching books
+const searchBook = () => {
+    const searchText = searchField.value;
+    const url = `https://openlibrary.org/search.json?q=javascript`;
+
+    fetch(url)
+        .then(res => res.json())
+        .then(data => console.log(data.docs[0].author_name[0]))
+}
+searchBook();
